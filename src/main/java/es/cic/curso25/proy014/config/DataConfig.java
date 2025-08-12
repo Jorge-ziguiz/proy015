ce4f15d86d52cd6aec5398199c3f69538f430530
Jorge <jolgeelcritico@gmail.com>
2025-08-12 10:33:54 +0200

    mejoras de mantenibilidad

--- src/main/java/es/cic/curso25/proy014/config/DataConfig.java
+++ src/main/java/es/cic/curso25/proy014/config/DataConfig.java
@@ -16,13 +16,13 @@ public class DataConfig {
     public ApplicationRunner llenarGaraje(PlazaService plazaService) {
 
         return args -> {
-            int NumeroDePLazas = PlazaService.NUMERO_PLAZAS;
+            int numeroPLazas = PlazaService.NUMERO_PLAZAS;
 
             List<Plaza> plazas = plazaService.getAll();
 
-            if (plazas.isEmpty() == true) {
+            if (plazas.isEmpty()) {
 
-                for (int i = 0; i < NumeroDePLazas; i++) {
+                for (int i = 0; i < numeroPLazas; i++) {
                     Plaza plaza = new Plaza();
                     plazaService.create(plaza);
                 }
